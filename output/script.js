f9f2089010673ec058074aeef3bd311c2491a294
abdullah-al-masum-24 <115403587+abdullah-al-masum-24@users.noreply.github.com>
2022-10-29 21:59:57 +0600

    project
    project

---
+++ output/script.js
@@ -0,0 +1,33 @@
+"use strict";
+const age = 23; // My Age
+let calresult;
+let returnresult;
+let finalresult;
+// This function is calling to show calculation result.
+resultshow(2, 5, '*');
+// This is a Calculation Result Showing Function
+function resultshow(firstinput, secondinput, operator) {
+    returnresult = calculation(firstinput, secondinput, operator);
+    finalresult = returnresult + age;
+    console.log('Result = ' + finalresult);
+}
+// This is a Calculation Function
+function calculation(firstinput, secondinput, operator) {
+    // Operator Check for Calculation
+    if (operator === '+') {
+        calresult = firstinput + secondinput;
+    }
+    else if (operator === '-') {
+        calresult = firstinput - secondinput;
+    }
+    else if (operator === '*') {
+        calresult = firstinput * secondinput;
+    }
+    else if (operator === '/') {
+        calresult = firstinput / secondinput;
+    }
+    else {
+        console.log('Please input any operator for calculation !!!!');
+    }
+    return calresult;
+}
---
+++ src/script.ts
@@ -0,0 +1,43 @@
+const age = 23;         // My Age
+let calresult : number;
+let returnresult : number;
+let finalresult : number;
+
+// This function is calling to show calculation result.
+resultshow(2, 5, '*' );
+
+// This is a Calculation Result Showing Function
+function resultshow( firstinput: number, secondinput: number, operator: string ) {
+
+    returnresult = calculation( firstinput, secondinput, operator);
+    finalresult  = returnresult + age;
+
+    console.log( 'Result = '+ finalresult );
+}
+
+// This is a Calculation Function
+function calculation( firstinput: number, secondinput: number, operator: string  ) {
+
+    // Operator Check for Calculation
+    if ( operator === '+' ) {
+
+        calresult = firstinput + secondinput;
+
+    }else if ( operator === '-' ) {
+
+        calresult = firstinput - secondinput;
+
+    }else if ( operator === '*' ) {
+
+        calresult = firstinput * secondinput;
+
+    }else if ( operator === '/' ) {
+
+        calresult = firstinput / secondinput;
+
+    }else {
+       console.log('Please input any operator for calculation !!!!')
+    }
+
+    return calresult;
+}
